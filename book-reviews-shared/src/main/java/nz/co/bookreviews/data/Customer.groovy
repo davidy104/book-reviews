aa53f91732bd89246cac53178bc1f2ad62b27045
davidy <david.yuan124@gmail.com>
2014-09-28 08:01:16 +1300

    test

--- book-reviews-shared/src/main/java/nz/co/bookreviews/data/Customer.groovy
+++ book-reviews-shared/src/main/java/nz/co/bookreviews/data/Customer.groovy
@@ -2,10 +2,11 @@ package nz.co.bookreviews.data
 
 import groovy.transform.ToString
 
+import org.codehaus.jackson.annotate.JsonAutoDetect
 import org.codehaus.jackson.annotate.JsonIgnore
 
 @ToString(includeSuper=true, includeNames=true)
+@JsonAutoDetect(isGetterVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.ANY)
 class Customer extends Person {
-	@JsonIgnore
 	boolean member
 }
