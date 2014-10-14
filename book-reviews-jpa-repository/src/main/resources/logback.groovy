20ae0634e8f8a0c5c9f19b7aa6b8822788605441
davidy <david.yuan@propellerhead.co.nz>
2014-10-14 14:53:49 +1300

    test

---
+++ book-reviews-jpa-repository/src/main/resources/logback.groovy
@@ -0,0 +1,23 @@
+//
+// Built on Tue Oct 14 00:56:44 CEST 2014 by logback-translator
+// For more information on configuration files in Groovy
+// please see http://logback.qos.ch/manual/groovy.html
+
+// For assistance related to this tool or configuration files
+// in general, please contact the logback user mailing list at
+//    http://qos.ch/mailman/listinfo/logback-user
+
+// For professional support please see
+//   http://www.qos.ch/shop/products/professionalSupport
+
+import static ch.qos.logback.classic.Level.INFO
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+appender("STDOUT", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{5} - %msg%n"
+  }
+}
+logger("nz.co.bookreviews", INFO, ["STDOUT"], false)
+root(INFO, ["STDOUT"])
